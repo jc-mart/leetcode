66137d3e08ba80f01d9a342b96e5ce1ed4c5370f
Juan C Martinez <git@jcmart.dev>
2025-05-21 15:29:15 -0500

    Solved 3

--- 3_longestsubstr.c
+++ 3_longestsubstr.c
@@ -1,39 +1,20 @@
 int lengthOfLongestSubstring(char* s) {
-    int ans = 0, obs = 0, dup = 0, p1 = 0, p2 = 0, size = 0;
+    int ans = 0, obs = 0, dup = 0, p1 = 0, p2 = 0;
+        
+    while (s[p2] != '\0') {
+        obs++; 
 
-    while (s[p1] != '\0')
-        p1++;
-
-    size = p1; // Does not count null operator
-    p1 = 0;
-    p2++;
-    printf("Size: %d\n", size);
-
-    if (size != 0) {
-        obs++, ans++;
-        for (; size - p2 >= ans; p2++, obs++) {
-            for (int i = p1 + 1; i <= p2; i++) {
-                if (s[i] == s[p1]) {
-                    printf("Found dupe\n");
-                    dup++;
-                    // break;
-                }
+        for (int i = p1, mov = 1; i < p2; i++, mov++) {
+            if (s[i] == s[p2]) { // Duplicate was found
+                obs -= mov;
+                p1 += mov;
             }
-
-            if (dup) {
-                printf("Elimniating dupe\n");
-                p1++;
-                obs--;
-                dup--;
-            }
-
-
-            if (obs > ans)
-                ans = obs;
-
-            printf("ans: %d, obs: %d, dup: %d, p1: %d, p2: %d\n",
-                    ans, obs, dup, p1, p2);
         }
+
+        if (obs > ans)
+            ans = obs;
+
+        p2++;
     }
 
     return ans;
