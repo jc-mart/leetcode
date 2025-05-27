71b805c80125398fb9a106cfe8842dd6fe48a15e
Juan C Martinez <git@jcmart.dev>
2025-05-27 13:17:47 -0500

    Solved 5 but can improve

--- daily/5_longestpalindromic.c
+++ daily/5_longestpalindromic.c
@@ -1,17 +1,36 @@
 char* longestPalindrome(char* s) {
-    int p1, p2, count = 0, ans = 0;
-    int size = sizeof(s) / sizeof(s[0]);
+    int ls, rs, l, r, count, top = 0;
+    int size = strlen(s);
+    char *ans = (char *)malloc(2 * sizeof(char));
 
-    for (int i = 0; i <= size; i++) {
-        for (int j = i; j <= size; j++) {
-            p1 = i, p2 = j;
-            while (p1 < j && p2 > i) { // While within the sliding window
-                if (s[p1] != s[p2]) {
-                    count = 0;
-                    break;
+    ans[0] = s[0];
+    ans[1] = '\0';
+
+    if (size > 1) {
+        for (int i = 0; i < size; i++) { // i holds window size
+            for (ls = 0, rs = i; rs < size; rs++, ls++) { // Moves window
+                count = 0;
+                l = ls, r = rs;
+
+                while (l <= r && l < size && r > 0) {
+                    if (s[l] == s[r])
+                        count = (l != r) ? count + 2 : count + 1;
+                    else {
+                        count = 0;
+                        break;
+                    }
+                    l++, r--;
                 }
 
-                count += 2;
+                if (count > top) {
+                    top = count;
+                    ans = (char *)malloc((top + 1) * sizeof(char));
+
+                    for (int j = ls, k = 0; j <= rs; j++, k++) {
+                        ans[k] = s[j];
+                    }
+                    ans[top] = '\0';
+                }
             }
         }
     }
